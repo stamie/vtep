eac1d410b0ec69c6c8aa4cb3869cea8230f24982
stamie <estampel@gmail.com>
2025-05-20 18:36:36 +0200

    Add project files.

---
+++ vtep/FormVtep1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace vtep
+{
+    partial class FormVtep
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxVtep1 = new System.Windows.Forms.TextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonSaveTextBoxVtep1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxVtep1
+            // 
+            this.textBoxVtep1.Location = new System.Drawing.Point(51, 37);
+            this.textBoxVtep1.Multiline = true;
+            this.textBoxVtep1.Name = "textBoxVtep1";
+            this.textBoxVtep1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxVtep1.Size = new System.Drawing.Size(680, 298);
+            this.textBoxVtep1.TabIndex = 0;
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(51, 368);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(107, 23);
+            this.buttonGo.TabIndex = 1;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(608, 368);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(123, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonSaveTextBoxVtep1
+            // 
+            this.buttonSaveTextBoxVtep1.Location = new System.Drawing.Point(290, 368);
+            this.buttonSaveTextBoxVtep1.Name = "buttonSaveTextBoxVtep1";
+            this.buttonSaveTextBoxVtep1.Size = new System.Drawing.Size(176, 23);
+            this.buttonSaveTextBoxVtep1.TabIndex = 3;
+            this.buttonSaveTextBoxVtep1.Text = "Save Text";
+            this.buttonSaveTextBoxVtep1.UseVisualStyleBackColor = true;
+            this.buttonSaveTextBoxVtep1.Click += new System.EventHandler(this.buttonSaveText_Click);
+            this.buttonSaveTextBoxVtep1.Visible = false;
+            this.buttonSaveTextBoxVtep1.Enabled = false;
+            // 
+            // FormVtep
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSaveTextBoxVtep1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.textBoxVtep1);
+            this.Name = "FormVtep";
+            this.Text = "vtep";
+            this.Load += new System.EventHandler(this.FormVtep_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxVtep1;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonSaveTextBoxVtep1;
+    }
+}
---
+++ vtep/FormVtep1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GetID;
+namespace vtep
+{
+    public partial class FormVtep : Form
+    {
+        private const string NotGood = "Nem megfelelő";
+        private const string Good = "Megfelelő";
+        private const string Pattern = @"^[A-Z][0-9]{4}$";
+        private bool stopButtonClicked = false;
+        private bool goButtonClicked = false;
+        private bool saveButtonClicked = false;
+        private BackgroundWorker backgroundWorkerGoEvent = new BackgroundWorker();
+        private BackgroundWorker backgroundWorkerSaveTextEvent = new BackgroundWorker();
+        private GetID.GetID Ids;
+        public PropertyChangedEventHandler ValueChanged2;
+        private string textVtepForm = "";
+        public FormVtep()
+        {
+            InitializeComponent();
+            backgroundWorkerGoEvent.DoWork += backgroundWorkerGoEvent_DoWork;
+            backgroundWorkerGoEvent.WorkerReportsProgress = true;
+            backgroundWorkerSaveTextEvent.DoWork += backgroundWorkerSaveTextEvent_DoWork;
+            backgroundWorkerSaveTextEvent.WorkerReportsProgress = true;
+        }
+        private string isIdValidToString(string value)
+        {
+            Regex regex = new Regex(Pattern);
+            if (regex.IsMatch(value))
+            {
+                return " " + Good;
+            }
+            else
+            {
+                return " " + NotGood;
+            }
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            if (this.Ids == null)
+            {
+                this.Ids = new GetID.GetID();
+            }
+            if (!backgroundWorkerGoEvent.IsBusy && !this.goButtonClicked) {
+                this.Ids.Go();
+                this.stopButtonClicked = false;
+                this.goButtonClicked = true;
+                MessageBox.Show("Go button clicked");
+                backgroundWorkerGoEvent.RunWorkerAsync();
+            }
+
+        }
+        public void backgroundWorkerGoEvent_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (this.goButtonClicked)
+            {
+                Thread.Sleep(50);
+                this.Ids_ValueChanged();
+                if (this.stopButtonClicked)
+                {
+                    this.goButtonClicked = false;
+                    return;
+                }
+            }
+            return;
+        }
+        private void Ids_ValueChanged()
+        {
+            try
+            {
+                string value = this.Ids.Value;
+                if (value != null)
+                {
+                    this.textVtepForm += Environment.NewLine + value + this.isIdValidToString(value.Trim());
+                    this.textVtepForm = this.textVtepForm.Trim();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (!this.stopButtonClicked)
+            {
+                MessageBox.Show("Stop button clicked");
+                this.stopButtonClicked = true;
+                this.goButtonClicked = false;
+                this.textBoxVtep1.Text = this.textVtepForm;
+                this.buttonSaveTextBoxVtep1.Enabled = true;
+                this.buttonSaveTextBoxVtep1.Visible = true;
+                this.Ids.Stop();
+            }
+        }
+        public void backgroundWorkerSaveTextEvent_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                string fileName = "vtep.txt";
+                System.IO.File.WriteAllText(fileName, this.textVtepForm);
+                MessageBox.Show("File saved as " + fileName);
+                this.saveButtonClicked = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                this.saveButtonClicked = false;
+            }
+
+            return;
+        }
+        private void buttonSaveText_Click(object sender, EventArgs e)
+        {
+            if (!this.saveButtonClicked)
+            {
+                DialogResult dialogResultSave = MessageBox.Show("Save button clicked");
+                this.saveButtonClicked = true;
+                backgroundWorkerSaveTextEvent.RunWorkerAsync();
+            }
+        }
+        private void FormVtep_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
